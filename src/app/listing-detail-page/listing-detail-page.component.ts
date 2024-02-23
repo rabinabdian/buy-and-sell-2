efff117cb6ab342d37171ada18b1379ec98c1b4d
rabinabdian <rabin.abdian@gmail.com>
2024-02-23 17:35:22 +0200

    listing page with details

--- src/app/listing-detail-page/listing-detail-page.component.ts
+++ src/app/listing-detail-page/listing-detail-page.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Listing } from './../types';
+import { fakeListings } from './../fake-data';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 
@@ -7,12 +9,15 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './listing-detail-page.component.html',
   styleUrl: './listing-detail-page.component.scss'
 })
-export class ListingDetailPageComponent {
+export class ListingDetailPageComponent implements OnInit {
+listing: Listing | undefined;
+
 
   constructor(private route: ActivatedRoute,){}
 
   ngOnInit(): void {
     const id= this.route.snapshot.paramMap.get('id');
+    this.listing = fakeListings.find(listing => listing.id===id);
   }
 
 }
